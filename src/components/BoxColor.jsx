afbd0a7582e874c63237d9de5df19eb5cb78c903
Raquel Benito <raquelbenitop@gmail.com>
2024-07-16 19:49:05 +0200

    First commit

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,15 @@
-function boxColor ({color, value}) {
+function BoxColor ({color, value}) {
+  const check = color === value
   return (
     <>
+      <div style={{
+            backgroundColor: check ? color : 'transparent',
+            color: check ? 'black' : color,
+            border: `1px solid ${color}`,
+      }}>
+        <p>{check ? `Yes, I'm a ${color} color` : `No, I'm not a ${value} color`}</p>
+      </div>
     </> 
   )
 }
-export default boxColor;  
+export default BoxColor;  
--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,9 +1,23 @@
-
+import { useEffect, useState, useRef } from "react"
+import BoxColor from "./BoxColor";
 
 function MyForm() {
+  const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive']
+
+  const [value, setValue] = useState("")
+  const inputRef = useRef(null)
 
   return (
-    <></>
+    <>
+      <input id="color" type="text" className="input" placeholder="Añade el color" onChange={()=> setValue(inputRef.current.value)} value={value} ref={inputRef}/>
+      <div>{value}</div>
+
+      <div className="container">
+        {colors.map((color, i) => (
+          <BoxColor key={i} className="box" color={color} value={value} />
+        ))}
+      </div>
+    </>
   );
 }
 
